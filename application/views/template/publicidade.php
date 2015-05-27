e551dd90a77e07ae0172bcea709a1cd05362f82a
helderhlp <helder.developer@gmail.com>
2015-05-27 14:46:31 -0300

    Fix

--- application/views/template/publicidade.php
+++ application/views/template/publicidade.php
@@ -2,7 +2,7 @@
     <div class="center-align">
         <h2>PUBLICIDADE</h2>
         <div class="publicidade left"><a target="_blank" href="https://delavada.wordpress.com/"><img src="<?php print base_url(); ?>uploads/publicidade/delavada.jpg" alt="De Lavada"></a></div>
-        <div class="publicidade center"><a href=""><img src="<?php print base_url(); ?>uploads/publicidade/joguela.jpg" alt="Jogue Lá - Aluguel de Quadras Society"></a></div>
+        <div class="publicidade center"><a href="<?php print base_url(); ?>cadastre-seu-local"><img src="<?php print base_url(); ?>uploads/publicidade/joguela.jpg" alt="Jogue Lá - Aluguel de Quadras Society"></a></div>
         <div class="publicidade right"><a target="_blank" href="https://socio-palmeiras.futebolcard.com/"><img src="<?php print base_url(); ?>uploads/publicidade/avante.jpg" alt="Banner"></a></div>
     </div>
 </div>
